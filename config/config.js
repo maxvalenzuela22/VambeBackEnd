4b57f9ecb0282fed7f8a40fbf070a01b0639da76
MAX VALENZUELA <max.valenzuela@uc.cl>
2025-02-26 16:42:34 -0300

    fix: config with production

--- config/config.js
+++ config/config.js
@@ -9,4 +9,14 @@ module.exports = {
     dialect: 'postgres',
     port: process.env.DB_PORT
   },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
 };
